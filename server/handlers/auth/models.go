2995b556bb8dba9612ee06bbdc8b4f0440b83dd9
danields <danields761@gmail.com>
2018-07-11 17:30:07 +0300

    fix(auth): fix password confirmation in signup request

--- server/handlers/auth/models.go
+++ server/handlers/auth/models.go
@@ -3,8 +3,8 @@ package auth
 // UserSignupRequest represents user signup request
 type UserSignupRequest struct {
 	Phone                string  `validate:"required,min=5" json:"phone"`
-	Password             string  `validate:"required,min=5,eqfield=Password" json:"password"`
-	PasswordConfirmation string  `validate:"required" json:"password_confirmation" `
+	Password             string  `validate:"required,min=5" json:"password"`
+	PasswordConfirmation string  `validate:"required,eqfield=Password" json:"password_confirmation" `
 	ReferrerPhone        *string `json:"referrer_phone"`
 }
 
