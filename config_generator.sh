d9c13c22e3f422db08cec78ba50ee31ca8565586
danields <danields761@gmail.com>
2018-07-20 12:50:31 +0300

    feat(ci): new config generation on staging

---
+++ config_generator.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+echo 'env: staging
+db:
+    uri: '$STAGING_DB_URI'
+server:
+    storage:
+        uri: '$STAGING_REDIS_URI'
+    auth:
+        tokenstorage: jwtpersistent
+        generator:
+            codealphabet: 4
+        jwt:
+            secret: '$STAGING_SECRET'
+            method: HS256'
